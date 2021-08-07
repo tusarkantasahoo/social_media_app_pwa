0df9b0c3e1a501c980f2063e5da77419d147c9b5
Tusar Kanta Sahoo <tusar.kanta.sahoo@ericsson.com>
2021-08-07 23:18:42 +0530

    Api for Survey integrated

--- src/app/api/Api.js
+++ src/app/api/Api.js
@@ -114,3 +114,28 @@ export function deletePostById(id) {
       });
   });
 }
+
+export function createSurvey(payload) {
+  console.log("===");
+  var config = {
+    method: "post",
+    url: getBaseUrl().baseUrl + "api/survey/create/",
+    headers: {
+       'Content-Type': 'Application/json'
+    },
+
+    data: payload,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        resolve(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+}
+
--- src/app/components/createSurvey/Poll.js
+++ src/app/components/createSurvey/Poll.js
@@ -1,21 +1,55 @@
 import React, { Component } from "react";
-
+import { authResponseStoredValue } from "../../../utils/Constant.js";
+import {createSurvey} from "../../api/Api.js";
 export default class Poll extends Component {
   constructor(props) {
     super(props);
     this.state = { 
         noOfOptions:2, 
         postText:props.postText,
+        option1:"",
+        option2:"",
+        option3:"",
+        option4:"",
+        user:JSON.parse(localStorage.getItem(authResponseStoredValue))
+
 
     };
+    this._onClickSurveySubmit = this._onClickSurveySubmit.bind(this);
   }
+
+  async _onClickSurveySubmit(){
+    var optionsArray = [];
+    if(this.state.noOfOptions===2){
+      optionsArray = [this.state.option1,this.state.option2]
+    }
+    if(this.state.noOfOptions===3){
+      optionsArray = [this.state.option1,this.state.option2,this.state.option3]
+    }
+    if(this.state.noOfOptions===4){
+      optionsArray = [this.state.option1,this.state.option2,this.state.option3,this.state.option4]
+    }
+
+    var surveyPayload={
+      title:this.state.postText,
+      surveyType:"poll",
+      options:optionsArray,
+      user:this.state.user.userData
+    }
+
+    var responsePollSurvey = await createSurvey(surveyPayload)
+    if(responsePollSurvey.status === 200){
+      console.log("response create survey",responsePollSurvey)
+    }
+  }
+
   render() {
       console.log("poll created");
     return (
       <>
         <div style={{ width: "90%", marginLeft: "5%" }}>
               <input
-                value={this.props.state.postText}
+                value={this.state.postText}
                 onChange={(e) => {
                   this.setState({ postText: e.target.value });
                 }}
@@ -49,7 +83,13 @@ export default class Poll extends Component {
                   border: "0.5px solid ",
                   outline: "0",
                 }}
+                value={this.state.option1}
+
                 placeholder="Choice 1"
+
+                onChange={(e) => {
+                  this.setState({ option1: e.target.value });
+                }}
               ></input>
               <input
                 style={{
@@ -58,6 +98,10 @@ export default class Poll extends Component {
                   border: "0.5px solid ",
                   outline: "0",
                   marginTop: "1em",
+                }}
+                value={this.state.option2}
+                onChange={(e) => {
+                  this.setState({ option2: e.target.value });
                 }}
                 placeholder="Choice 2"
               ></input>
@@ -73,6 +117,10 @@ export default class Poll extends Component {
                         marginTop: "1em",
                       }}
                       placeholder="Choice 3"
+                      value={this.state.option3}
+                      onChange={(e) => {
+                        this.setState({ option3: e.target.value });
+                      }}
                     ></input>
                   ) : (
                     <>
@@ -84,7 +132,11 @@ export default class Poll extends Component {
                           outline: "0",
                           marginTop: "1em",
                         }}
+                        value={this.state.option3}
                         placeholder="Choice 3"
+                        onChange={(e) => {
+                          this.setState({ option3: e.target.value });
+                        }}
                       ></input>
                       <input
                         style={{
@@ -94,7 +146,11 @@ export default class Poll extends Component {
                           outline: "0",
                           marginTop: "1em",
                         }}
+                        value={this.state.option4}
                         placeholder="Choice 4"
+                        onChange={(e) => {
+                          this.setState({ option4: e.target.value });
+                        }}
                       ></input>
                     </>
                   )}
@@ -118,13 +174,14 @@ export default class Poll extends Component {
             </div>
             <div style={{ marginTop: "1em" }}>
               <button
-                styl  e={{
+                style={{
                   border: "1px solid",
                   backgroundColor: "#1da1f2",
                   fontSize: "18px",
                   color: "white",
                   width: "50%",
                 }}
+                onClick={() => this._onClickSurveySubmit()}
               >
                 Post Survey
               </button>
